200daa233d6d503eb4cd8e089aa7f7cdbc0fd78b
Karolina Siwkowska <trallala00@gmail.com>
2018-05-17 18:33:53 +0200

    Add components folder, make Form.js and Weather.js,edit App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "38c812d3e2e2681470d9ca6a83c3c330";
 
 class App extends React.Component {
     render() {
@@ -8,6 +12,10 @@ class App extends React.Component {
             div >
             <
             Titles / >
+            <
+            Form / >
+            <
+            Weather / >
             <
             /div>
         );
