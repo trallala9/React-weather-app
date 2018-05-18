b8a3ddd987a966dc6450f89027b1a8bc4bc48802
Karolina Siwkowska <trallala00@gmail.com>
2018-05-18 20:34:31 +0200

    Add (e)to prevent default refreshing,change API URL,add async=await witch fetch

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
-const API_KEY = "38c812d3e2e2681470d9ca6a83c3c330";
+const API_KEY = "b589e3120ecca54bd814f1ad95ed162e";
 
 class App extends React.Component {
+    getWeather = async (e) => {
+        e.preventDefault();
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Poznan,pl&appid=${API_KEY}&units=metric`);
+        const data = await api_call.json();
+        console.log(data);
+    }
     render() {
         return ( <
             div >
             <
             Titles / >
             <
-            Form / >
-            <
+            Form getWeather = {
+                this.getWeather
+            }
+            / > <
             Weather / >
             <
             /div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,10 +3,22 @@ import React from "react";
 class Form extends React.Component {
 
     render() {
-        return ( <
-            div > Form Component < /div>
+        return ( < form onSubmit = {
+                this.props.getWeather
+            } >
+            <
+            input type = "text"
+            name = "city"
+            placeholder = "City..." / >
+            <
+            input type = "text"
+            name = "country"
+            placeholder = "Country..." / >
+            <
+            button > Get Weather < /button> < /
+            form >
         );
     }
 
-}
+};
 export default Form;
