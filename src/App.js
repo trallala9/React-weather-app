a1b9a33299b900ccb3ba826b990d23c0f1b5d898
Karolina Siwkowska <trallala00@gmail.com>
2018-05-19 00:08:27 +0200

    Add setState method,define prps in App.js,add props in Weather.js,Add &&operator to prevent display onload,add if and elese statements

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends React.Component {
     state = {
         temperature: undefined,
         city: undefined,
-
         country: undefined,
         humidity: undefined,
         description: undefined,
@@ -23,7 +22,26 @@ class App extends React.Component {
         const country = e.target.elements.country.value;
         const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
         const data = await api_call.json();
-        console.log(data);
+        if (city && country) {
+            console.log(data);
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                error: ""
+            });
+        } else {
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: "Please enter the value"
+            });
+        }
     }
     render() {
         return ( <
@@ -35,9 +53,26 @@ class App extends React.Component {
                 this.getWeather
             }
             / > <
-            Weather / >
-            <
-            /div>
+            Weather temperature = {
+                this.state.temperature
+            }
+            city = {
+                this.state.city
+            }
+            country = {
+                this.state.country
+            }
+            humidity = {
+                this.state.humidity
+            }
+            description = {
+                this.state.description
+            }
+            error = {
+                this.state.error
+            }
+            / > < /
+            div >
         );
     }
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,11 +2,34 @@ import React from "react";
 
 class Weather extends React.Component {
 
-    render() {
-        return ( <
-            div > Weather Component < /div>
-        );
-    }
+        render() {
+                return ( <
+                        div > {
+                            this.props.city && this.props.country && < p > Location: {
+                                this.props.city
+                            },
+                            {
+                                this.props.country
+                            } < /p>}  {
+                            this.props.temperature && < p > temperature: {
+                                this.props.temperature
+                            } < /p>} {
+                            this.props.humidity && < p > Humidity: {
+                                this.props.humidity
+                            } < /p>}
 
-}
-export default Weather;
+                            {
+                                this.props.description && < p > Conditions: {
+                                    this.props.description
+                                } < /p>} 
+
+                                {
+                                    this.props.error && < p > {
+                                        this.props.error
+                                    } < /p>}< /
+                                    div >
+                                );
+                            }
+
+                        };
+                        export default Weather;
