ae62edf5551d8564096a37c6d1842472c1c83d2f
Karolina Siwkowska <trallala00@gmail.com>
2018-05-18 21:43:28 +0200

    Add const city and country,chnge api_call URL adress for dynamic one,add state-an object to display values on the screen

--- src/App.js
+++ src/App.js
@@ -7,9 +7,21 @@ import Weather from "./components/Weather";
 const API_KEY = "b589e3120ecca54bd814f1ad95ed162e";
 
 class App extends React.Component {
+    state = {
+        temperature: undefined,
+        city: undefined,
+
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined
+
+    }
     getWeather = async (e) => {
         e.preventDefault();
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Poznan,pl&appid=${API_KEY}&units=metric`);
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
         const data = await api_call.json();
         console.log(data);
     }
